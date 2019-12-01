96b7c0b7d968f94a5413490f626fe3c938f2e7d3
Megharth <m.c.lakhataria@gmail.com>
2019-11-30 23:35:00 -0500

    add seeds script

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -18,6 +18,66 @@ alias Social.Configs.Config
 pw = Argon2.hash_pwd_salt("password")
 date = ~D[1999-05-18]
 
+usernames = [
+  "engagingunhinge",
+  "humorousprong",
+  "blathersperky",
+  "runiefetch",
+  "riflemiscreant",
+  "camcorderneatly",
+  "geniusderby",
+  "mankindsmake",
+  "amplifierslip",
+  "exaltationashamed",
+  "hatcherywaals",
+  "campuscathedral",
+  "llewelynline",
+  "opulentlycompromise",
+  "followingcool",
+  "equivocatechoir",
+  "karatedavy",
+  "ethiopianbaseball",
+  "updateeffects",
+  "particularrift",
+  "londondray",
+  "mythimpressive",
+  "hostilerates",
+  "rompconan",
+  "marquisace",
+  "earnestcavernous",
+  "comicplanck",
+  "harnessglen",
+  "darkishpedigree",
+  "gelsdwelling",
+  "piperrecently",
+  "johnslila",
+  "smackblast",
+  "careenattach",
+  "tingleyblackwell",
+  "fencemobile",
+  "sturdilyhalftime",
+  "thirstilylesser",
+  "lightscoaming",
+  "goodsjailer",
+  "throngsubscript",
+  "shunderyavalanche",
+  "studycoathanger",
+  "regretbickled",
+  "disinfectflail",
+  "camillapostcard",
+  "hisnonce",
+  "batsbroken",
+  "dadsecular",
+  "frostycomet",
+]
+
+lat = 42.3474498
+lng = -71.0854208
+
+Enum.each(usernames, fn username ->
+  Repo.insert!(%User{name: username, username: username, email: username <> "@test.com", dob: date, password_hash: pw, latitude: lat, longitude: lng})
+end)
+
 Repo.insert!(%User{name: "Test", username: "test", email: "test@test.com", dob: date, password_hash: pw, latitude: 42.3476498, longitude: -71.0854208})
 Repo.insert!(%User{name: "Test1", username: "test1", email: "test1@test.com", dob: date, password_hash: pw, latitude: 42.3474498, longitude: -71.0856208})
 Repo.insert!(%User{name: "Test2", username: "test2", email: "test2@test.com", dob: date, password_hash: pw, latitude: 42.3476498, longitude: -71.0856208})
