f0facfcbed6ccae57617e3613f2322a2636bb6bf
Nikhil <31805650+nikhilj13@users.noreply.github.com>
2019-12-01 21:11:11 -0500

    Request page styling

--- assets/js/components/requests.jsx
+++ assets/js/components/requests.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import { listRequests } from '../ajax';
-import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Button, Row } from 'react-bootstrap';
 import { changeRequest } from "../ajax";
 
 class Requests extends React.Component {
@@ -17,24 +18,30 @@ class Requests extends React.Component {
 
     
     render() {
-
         let request = [];
-        this.props.requests.map((req) => {
-            request.push(<div className="row" key={req.id}>
-            {req.name}
-            <div className="col">
-                <Button onClick={() => changeRequest(req.request_id, "ACCEPTED")}>Accept</Button></div>
-            <div className="col">
-                <Button onClick={() => changeRequest(req.request_id, "REJECTED")}>Reject</Button>
-            </div>
-        </div>);
+        this.props.requests.map((req, index) => {
+            request.push(
+                <div className="rec-user-card" style={{ width: '22em' }} key={req.id}>
+                    <Row>
+                        <div className="name" style={{ paddingLeft: '1em'}}>
+                            <Link to={"/user-profile/" + req.request_id}>{req.name}</Link>
+                        </div>
+                        <Button style={{ marginLeft: '3em'}} variant="none" className="btn btn-outline-social" onClick={() => changeRequest(req.request_id, "ACCEPTED")}>Accept</Button>
+                        <Button style={{ marginLeft: '3em'}} variant="outline-dark" onClick={() => changeRequest(req.request_id, "REJECTED")}>Reject</Button>
+                    </Row>
+                </div>
+            );
         });
         console.log("res",request);
 
-        return(<div className="container">
-            <div className="header"><h2>My Requests</h2></div>
-            {request}
-        </div>);
+        return(
+            <div className="container">
+                <Row style={{ paddingTop: '1em' }}>
+                    <div className="header"><h2>My Requests</h2></div>
+                </Row>
+                {request}
+            </div>
+        );
     }
 }
 
